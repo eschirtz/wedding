b012ac83570d942384797d62365d0ab5b7326c58
Eric Schirtzinger <eschirtzinger@gmail.com>
2022-10-23 15:57:44 -0700

    fix: deploy script

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -10,7 +10,7 @@ import BaseButton from "./components/BaseButton.vue"
     LaBarge + Schirtzinger
   </h1>  
   <p class="text-center text-sm mb-4">
-    We're getting married in Scottland! Let's see how this goes 🙈
+    We're getting married in Scottland! (Deploy via GitHub Actions)
   </p>
   <BaseButton
     label="RSVP"
