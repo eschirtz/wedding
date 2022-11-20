ce377ced069cd7d8f7c9c8ecc34ac22f34fc6231
Eric Schirtzinger <eschirtzinger@gmail.com>
2022-11-20 03:09:38 +0000

    mobile nav styles

--- webapp/src/components/MobileNavigationMenu.vue
+++ webapp/src/components/MobileNavigationMenu.vue
@@ -1,16 +1,16 @@
 <template>
   <div @click="$emit('click-outside', $event)">
-    <div class="bg-stone-900/40 backdrop-blur-lg flex flex-col gap-4 p-4 items-center">
-      <BaseButton label="RSVP" class="max-w-sm"/>
+    <div class="bg-stone-100 backdrop-blur-lg flex flex-col gap-4 p-6 items-center">      
       <RouterLink 
         v-for="route of routes" 
         :key="route.name" 
         :to="route.to" 
-        class="text-white uppercase self-center"
+        class="text-stone-600 uppercase self-center"
         @click.stop="$emit('toggle-menu')"
       >
         {{ route.name }}
       </RouterLink>    
+      <BaseButton label="RSVP" class="max-w-sm mt-4"/>
     </div>
   </div>
 </template>
