fbac7f920dec69d3e926c002bdfd28c98c66e982
Eric Schirtzinger <eschirtzinger@gmail.com>
2023-01-16 02:45:39 +0000

    feat: refactor story

--- webapp/src/components/StoryChapter.vue
+++ webapp/src/components/StoryChapter.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="grid grid-rows-2 md:grid-rows-none md:grid-cols-2 gap-10 my-32 justify-center max-w-4xl mx-auto">
-    <div class="flex">
-      <BaseImage style="width: 230px; height: 372px" src="hannah-surf.jpg" lazy-src="hannah-surf-lazy.jpg"
+  <div class="grid grid-rows-2 md:grid-rows-none md:grid-cols-2 gap-10 my-32 justify-items-center items-center max-w-6xl mx-auto">
+    <div class="flex w-full max-w-md">
+      <BaseImage style="width: 100%; height: 372px" src="hannah-surf.jpg" lazy-src="hannah-surf-lazy.jpg"
         class="rounded shadow hannah-surf" />
-      <BaseImage style="width: 230px; height: 372px" src="eric-surf.jpg" lazy-src="eric-surf-lazy.jpg"
+      <BaseImage style="width: 100%; height: 372px" src="eric-surf.jpg" lazy-src="eric-surf-lazy.jpg"
         class="rounded shadow eric-surf" />
     </div>
     <div class="writeup text-left">
-      <h4 class="font-display text-4xl mb-4">How they met</h4>
+      <h4 class="font-display text-4xl mb-6">{{ title }}</h4>
       <p class="font-body text-base text-black/50">
-        It was the summer of 2015 when Hannah & a few of her friends hopped on Eric's boat. She was relatively new to
-        the Lake Geneva area but Eric, born and raised on Geneva Lake, was able to show Hannah what #lakelife was all
-        about.
-        <br />
-        <br />
-        Hannah caught on fairly quickly to wake-boarding, but Eric exhibited tremendous patience while teaching Hannah
-        to wake-surf. It wasn't love at first sight, but it was the beginning of the rest of their lives.
+        <template v-for="p in story">  
+          <span>
+            {{ p }}
+          </span>
+          <br />
+          <br />
+      </template>
       </p>
     </div>
   </div>
@@ -23,6 +23,11 @@
 
 <script lang="ts" setup>
 import BaseImage from '@/components/BaseImage.vue';
+
+defineProps({
+  title: String,
+  story: Array as () => string[],  
+});
 </script>
 
 <style>
