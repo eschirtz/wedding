e360696b1ea1dcf7fa311eeb44138c4b76b4f1f3
Eric Schirtzinger <eschirtzinger@gmail.com>
2023-01-22 22:26:11 +0000

    thefriendship

--- webapp/src/components/StoryTheFriendship.vue
+++ webapp/src/components/StoryTheFriendship.vue
@@ -1,9 +1,26 @@
 <template>
-  <div class="flex w-full max-w-md">
-    <BaseImage style="width: 100%; height: 372px" src="hannah-surf.jpg" lazy-src="hannah-surf-lazy.jpg"
-      class="rounded shadow hannah-surf" />
-    <BaseImage style="width: 100%; height: 372px" src="eric-surf.jpg" lazy-src="eric-surf-lazy.jpg"
-      class="rounded shadow eric-surf" />
+  <div class="flex w-96 shrink-0 relative h-96">
+    <div class="absolute -top-8 right-0">
+      <BaseImage src="game-day.jpg" lazy-src="game-day-lazy.jpg" class="rounded shadow w-64 h-48 -rotate-6" />
+    </div>
+    <div class="absolute bottom-0 -right-4">
+      <BaseImage src="wat.jpg" lazy-src="wat-lazy.jpg" class="rounded shadow w-64 h-56 rotate-6" /> 
+    </div>
+    <div class="absolute top-4 -left-8">
+      <BaseImage src="party.jpg" lazy-src="party-lazy.jpg" class="rounded shadow w-56 h-60 -rotate-3" />
+    </div>
+    <div class="absolute -bottom-4 left-0">
+      <BaseImage src="passport.png" class="w-36 h-48 -rotate-12" contain /> 
+    </div>
+    <div class="absolute bottom-0 -left-20">
+      <BaseImage src="baht.png" class="w-12 h-16 -rotate-45" contain /> 
+    </div>
+    <div class="absolute -bottom-12 left-1/2">
+      <BaseImage src="10baht.png" class="w-16 h-28 -rotate-6" contain /> 
+    </div>
+    <div class="absolute -bottom-16 right-20">
+      <BaseImage src="baht.png" class="w-12 h-16 rotate-12" contain /> 
+    </div>    
   </div>
 </template>
 
