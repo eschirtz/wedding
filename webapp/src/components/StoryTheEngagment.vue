899f9907236b786ffaed6afecd858b9b627d7bbd
Eric Schirtzinger <eschirtzinger@gmail.com>
2023-01-22 21:49:25 +0000

    feat: tiktok embed

--- webapp/src/components/StoryTheEngagment.vue
+++ webapp/src/components/StoryTheEngagment.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex w-full max-w-md">
-    <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@eschirtz/video/7134979559920831787" data-video-id="7134979559920831787" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@eschirtz" href="https://www.tiktok.com/@eschirtz?refer=embed">@eschirtz</a> If you would have told me… <a title="proposal" target="_blank" href="https://www.tiktok.com/tag/proposal?refer=embed">#proposal</a> @Hannah LaBarge <a target="_blank" title="♬ Something in the Orange - Zach Bryan" href="https://www.tiktok.com/music/Something-in-the-Orange-7090587074499201025?refer=embed">♬ Something in the Orange - Zach Bryan</a> </section> </blockquote>
+  <div class="flex max-w-md w-fit">
+    <blockquote class="tiktok-embed m-0" cite="https://www.tiktok.com/@eschirtz/video/7134979559920831787" data-video-id="7134979559920831787" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@eschirtz" href="https://www.tiktok.com/@eschirtz?refer=embed">@eschirtz</a> If you would have told me… <a title="proposal" target="_blank" href="https://www.tiktok.com/tag/proposal?refer=embed">#proposal</a> @Hannah LaBarge <a target="_blank" title="♬ Something in the Orange - Zach Bryan" href="https://www.tiktok.com/music/Something-in-the-Orange-7090587074499201025?refer=embed">♬ Something in the Orange - Zach Bryan</a> </section> </blockquote>
   </div>
 </template>
 
@@ -13,6 +13,8 @@ defineProps({
 </script>
 
 <style scoped>
-
+  .tiktok-embed {
+    box-shadow: 0px 32px 16px white;    
+  }
 
 </style>
