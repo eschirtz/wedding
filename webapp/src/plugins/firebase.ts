e2d1fd30330df14ab73705000331462c2f371cc7
Eric Schirtzinger <eschirtzinger@gmail.com>
2023-02-05 19:37:02 +0000

    feat: basic price refactor

--- webapp/src/plugins/firebase.ts
+++ webapp/src/plugins/firebase.ts
@@ -27,10 +27,85 @@ export const db = firebase.firestore();
 export const authStateKnown = ref(false);
 export const currentUser = ref<firebase.User | null>(null);
 
-auth.onAuthStateChanged( user => {
+/** General logic for handling payments */
+
+const PRICE_ID = "price_1MDvxwBDWgeikaYAYQyzbLxL";
+const TEST_PRICE_ID = 'price_1MYDJoBDWgeikaYAI1YvW2o7';
+
+const priceId = ref('');
+export const priceAmount = ref(0);
+
+const isTest = false;
+
+function getPrice() {
+  const productId = 'prod_Mxrh5falnKQFrb';
+  db.collection('products').doc(productId).collection('prices').where('active', '==', true).get()
+    .then(snap => {
+      const priceSnap = snap.docs[0];      
+      priceId.value = isTest ? TEST_PRICE_ID : priceSnap.id; // Override here so we can still test asynchronously
+      priceAmount.value = priceSnap.data().unit_amount;
+    });
+}
+
+getPrice(); // Immediately invoke
+
+export const paymentStatusKnown = ref(false);
+export const paymentsReceived = ref<number>(0);
+
+export async function payForGuests(numberOfGuests: number) {
+  if (!currentUser.value) return new Error('User not logged in');
+  const docRef = await db
+    .collection('users')
+    .doc(currentUser.value.uid)
+    .collection("checkout_sessions")
+    .add({
+      mode: "payment",
+      price: priceId.value,
+      quantity: numberOfGuests,
+      success_url: window.location.href,
+      cancel_url: window.location.href,
+    });
+  return new Promise<void>((resolve, reject) => {
+    docRef.onSnapshot(snap => {
+      const { error, url } = snap.data() as {
+        error: { message: string };
+        url: string;
+      };
+      if (error) {
+        // Show an error to your customer and
+        // inspect your Cloud Function logs in the Firebase console.
+        return reject(error.message);
+      }
+      if (url) {
+        window.location.assign(url);
+        return resolve();
+      }
+    });
+  });
+}
+
+/**
+ * Fetch list of payments for a user
+ * @param uid 
+ */
+function getUserPayments(uid: string) {
+  db.collection("users").doc(uid).collection("payments")
+    .where("status", "==", 'succeeded').onSnapshot(snap => {
+      paymentsReceived.value = snap.docs.length;
+      const paymentAmountReceived = snap.docs.reduce((acc, doc) => {
+        const data = doc.data();
+        return acc + data.amount_received;
+      }, 0);
+      console.log("Payments received:", paymentsReceived.value);
+      console.log("Payment amount received:", paymentAmountReceived);
+    });
+}
+
+auth.onAuthStateChanged(user => {
   currentUser.value = user;
   authStateKnown.value = true;
-})
+  if (user) getUserPayments(user.uid);
+});
 
 
 
--- webapp/src/views/Account.vue
+++ webapp/src/views/Account.vue
@@ -1,90 +1,54 @@
 <template>
   <div class="view-margin">
-    <h1 class="text-white text-6xl mb-4">
-      {{ currentUser?.displayName }}
-    </h1>
-    <p class="text-white mb-4">
+    <p class="mb-4">
       {{ priceId }} {{ priceAmount }}
     </p>
-    <input placeholder="Number of guests" v-model="quantity"/>
+    <input placeholder="Number of guests" v-model="quantity" />
     <BaseButton label="Checkout" @click="onCheckout" />
-    <BaseButton label="Logout" @click="logout"/>
+    <BaseButton label="Logout" @click="logout" />
+    <span v-if="checkoutLoading">Loading...</span>
+    <p>Paid for {{ paymentsReceived }} guests</p>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { onMounted, ref } from 'vue';
 import BaseButton from '../components/BaseButton.vue';
-import { currentUser, auth, db } from '../plugins/firebase';
+import { currentUser, auth, payForGuests, paymentStatusKnown, paymentsReceived, priceAmount } from '../plugins/firebase';
 import router from '../plugins/router';
 
-  defineProps({
-    //
-  });
+defineProps({
+  //
+});
 
-  defineEmits({
-    //
-  });
+defineEmits({
+  //
+});
 
-  const priceId = ref<string>();
-  const priceAmount = ref<number>();
-  const quantity = ref<number>(1);
+const priceId = ref<string>();
+// const priceAmount = ref<number>();
+const quantity = ref<number>(1);
+const checkoutLoading = ref(false);
 
-  async function getPrices() {
-    db.collection('products')
-    .where('active', '==', true)
-    .get()
-    .then(snap => {
-      snap.forEach(async doc => {
-        const priceSnap = await doc.ref.collection('prices').where('active', '==', true).get();
-        priceId.value = priceSnap.docs[0].id;
-        priceSnap.docs.forEach((doc) => {
-          priceAmount.value = doc.data().unit_amount;
-        });
-      });
-    });
+async function logout() {
+  await auth.signOut();
+  router.push('/');
+}
+
+async function onCheckout() {
+  checkoutLoading.value = true;
+  try {
+    await payForGuests(quantity.value);
+  } catch (error) {
+    alert(error);
+  } finally {
+    checkoutLoading.value = false;
   }
+}
 
-  async function logout() {
-    await auth.signOut();
-    router.push('/');
-  }
-
-  async function onCheckout() {    
-    const currentUser = auth.currentUser;
-    if(!currentUser) return new Error('User not logged in');
-    const docRef = await db
-    .collection('guests')
-    .doc(currentUser.uid)
-    .collection("checkout_sessions")
-    .add({
-      mode: "payment",
-      price: priceId.value,
-      quantity: quantity.value,
-      success_url: window.location.origin,
-      cancel_url: window.location.origin,
-    });
-    docRef.onSnapshot(snap => {
-      console.log(snap.data());
-      
-    const { error, url } = snap.data() as {
-      error: { message: string };
-      url: string;
-    };
-    if (error) {
-      // Show an error to your customer and
-      // inspect your Cloud Function logs in the Firebase console.
-      alert(`An error occured: ${error.message}`);
-    }
-    if (url) {
-      window.location.assign(url);
-    }
-  });
-  }
-
-  onMounted(() => {
-    getPrices();
-  });
+onMounted(() => {
+  // getPrices();
+});
 </script>
 
 <style scoped>
