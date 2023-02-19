11157a669cd3065637f8f99b05aff07121b3724d
Eric Schirtzinger <eschirtzinger@gmail.com>
2023-02-19 21:55:46 +0000

    feat: form styles

--- webapp/src/components/Guest.vue
+++ webapp/src/components/Guest.vue
@@ -1,18 +1,31 @@
 <template>
-  <form @submit.prevent @input="emit('update', $event, guest.id)" class="flex flex-col gap-2 py-6">
-    <TextInput label="Name" name="name" placeholder="First Last" :value="guest.data().name" class="mb-4" />
-    <TextInput label="Email" name="email" placeholder="example@email.com" :value="guest.data().email" class="mb-4" />
-    <TextInput label="Notes" name="notes" placeholder="Allergies, accessibility requirements, shared room preferences, etc..." :value="guest.data().notes" class="mb-4" />
-    <p v-if="confirmed">Confirmed!</p>
-    <BaseButton v-if="removable" label="Remove guest" @click="emit('remove', guest.id)" />
-  </form>
+  <div class="pb-6">
+    <div class="py-6 flex items-center justify-between" @click.prevent="open = !open">
+      <span class="flex items-center gap-2">
+        <h2 class="text-lg font-bold">
+          {{ guest.data().name || 'New guest' }}
+        </h2>
+        <Icon v-if="confirmed" icon="mdi:check-decagram" width="16px" height="16px" />
+      </span>
+      <Icon :icon="open ? 'mdi:chevron-down' : 'mdi:chevron-up'" width="16px" height="16px" />
+    </div>
+    <form v-if="open" @submit.prevent @input="emit('update', $event, guest.id)" class="flex flex-col gap-2">
+      <TextInput label="Name *" name="name" placeholder="First Last" :value="guest.data().name" class="mb-4 w-96" />
+      <TextInput label="Email *" name="email" placeholder="example@email.com" :value="guest.data().email" class="mb-4 w-96" />
+      <TextInput label="Flight info" name="flight" placeholder="AA 774 (You can add this later)" :value="guest.data().flight" class="mb-4 w-96" />
+      <TextInput label="Notes" name="notes" placeholder="Allergies, accessibility requirements, etc." :value="guest.data().notes" class="mb-4 w-96" />
+      <p v-if="confirmed">Confirmed!</p>    
+    </form>
+    <BaseButton v-if="removable" label="Remove guest" @click="emit('remove', guest.id)" @submit.prevent class="mt-2" />
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { PropType } from 'vue';
+import { PropType, ref } from 'vue';
 import firebase from 'firebase/app';
 import BaseButton from './BaseButton.vue';
 import TextInput from './TextInput.vue';
+import Icon from './Icon.vue';
 
 defineProps({
   guest: {
@@ -27,6 +40,9 @@ const emit = defineEmits({
   'update': (Event, string) => true,
   'remove': (string) => true,
 });
+
+const open = ref(true);
+
 </script>
 <style scoped>
 
--- webapp/src/components/TextInput.vue
+++ webapp/src/components/TextInput.vue
@@ -2,7 +2,7 @@
   <div>
   <label :for="name" class="block text-sm font-medium text-black/50">{{ label }}</label>
   <div class="relative mt-1 rounded-md shadow-sm">
-    <input type="text" :placeholder="placeholder" :name="name" :value="value" class="block w-full rounded-md border border-black/25 py-2 px-3 focus:border-amber-500 focus:ring-amber-500 sm:text-sm" />
+    <input type="text" :placeholder="placeholder" :name="name" :value="value" class="block w-full rounded-md border border-black/25 py-2 px-3 focus:border-amber-500 focus:ring-amber-500 sm:text-sm placeholder-gray-400" />
   </div>
 </div>  
 </template>
